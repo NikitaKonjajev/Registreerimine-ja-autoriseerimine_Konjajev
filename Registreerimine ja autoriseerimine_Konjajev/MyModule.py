0ed71d39da43fd9df0e604374eb90f6a503a2532
opilane <opilane@HP-CZC2349HV1>
2023-02-14 13:18:54 +0200

    123

--- Registreerimine ja autoriseerimine_Konjajev/MyModule.py
+++ Registreerimine ja autoriseerimine_Konjajev/MyModule.py
@@ -1,1 +1,24 @@
-
+from random import *
+def kasutajaandmed():
+    print("Kas sa tahad oma andmet või random")
+    a=input("Mida te soovite: ")
+    if a=="oma":
+        login=input("Kirjutage teie nimi: ")
+        password=input("Kirjutage teie salasõna: ")
+    elif a=="random":
+        str0=".,:;!_*-+()/#¤%&"
+        str1 = '0123456789'
+        str2 = 'qwertyuiopasdfghjklzxcvbnm'
+        str3 = str2.upper()
+        print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+        str4 = str0+str1+str2+str3
+        print(str4)
+        ls = list(str4)
+        print(ls)
+        shuffle(ls)
+        print(ls)
+        # Извлекаем из списка 12 произвольных значений
+        psword = ''.join([choice(ls) for x in range(12)])
+        # Пароль готов
+        print(psword)
+    return login,password
--- Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
+++ Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
@@ -1,1 +1,18 @@
+from MyModule import *
+#Registreerimine ja autoriseerimine
 
+login=[]
+password=[]
+while True:
+    print("Kas sa reg või log või välja")
+    valik=input("Mida te tahate: ")
+    if valik=="reg":
+         print("Registreerimine")
+         login,password=kasutajaandmed()
+         print("Teie login ja password",login,password)
+    elif valik=="aut":
+        pass 
+    elif valik=="välja":
+        print("Nägemist")
+        break
+        
