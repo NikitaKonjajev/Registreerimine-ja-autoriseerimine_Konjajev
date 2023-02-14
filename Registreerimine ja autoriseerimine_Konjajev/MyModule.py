a8b5fd8ed4135712ad4de94bbb5035a81966d3de
opilane <opilane@HP-CZC2349HV1>
2023-02-14 14:15:55 +0200

    123

--- Registreerimine ja autoriseerimine_Konjajev/MyModule.py
+++ Registreerimine ja autoriseerimine_Konjajev/MyModule.py
@@ -3,22 +3,35 @@ def kasutajaandmed():
     print("Kas sa tahad oma andmet või random")
     a=input("Mida te soovite: ")
     if a=="oma":
-        login=input("Kirjutage teie nimi: ")
-        password=input("Kirjutage teie salasõna: ")
+        login=input("Kirjutage teie nimi: ")       
+        psword=input("Kirjutage teie salasõna min6 värtused ja max 12 värtused: ")
+        n=len(psword)
+        while n>5 and n<12:
+            psword=input("Teie salasõna on lühike. Palun kirjuta veel: ")
+            n=len(psword)
     elif a=="random":
+        login=input("Kirjutage teie nimi: ")
         str0=".,:;!_*-+()/#¤%&"
         str1 = '0123456789'
         str2 = 'qwertyuiopasdfghjklzxcvbnm'
         str3 = str2.upper()
-        print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+        #print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
         str4 = str0+str1+str2+str3
-        print(str4)
+        #print(str4)
         ls = list(str4)
-        print(ls)
+        #print(ls)
         shuffle(ls)
-        print(ls)
+        #print(ls)
         # Извлекаем из списка 12 произвольных значений
         psword = ''.join([choice(ls) for x in range(12)])
-        # Пароль готов
-        print(psword)
-    return login,password
+    return login,psword
+ 
+def aut(ll:list,p:list):
+    print("Kirjutage teie login j salasõna")
+    logg=input("Login: ")
+    pas_=input("Salasõna: ")
+    if logg in ll and pas_ in p:
+        print("Tere tulemast")
+    else:
+        print("Ebaõiged andmed")
+    return logg, pas_
--- Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
+++ Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
@@ -1,17 +1,18 @@
 from MyModule import *
 #Registreerimine ja autoriseerimine
 
-login=[]
-password=[]
+ll=["user1","user2","user3"]
+p=["123","1234","321"]
 while True:
-    print("Kas sa reg või log või välja")
+    print("Kas sa reg või aut või välja")
     valik=input("Mida te tahate: ")
     if valik=="reg":
          print("Registreerimine")
-         login,password=kasutajaandmed()
-         print("Teie login ja password",login,password)
+         login,psword=kasutajaandmed()
+         print(f"Teie login ({login}) ja password ({psword})")
     elif valik=="aut":
-        pass 
+        print("autoriseerimine") 
+        log,pas=aut(ll,p)
     elif valik=="välja":
         print("Nägemist")
         break
