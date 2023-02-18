896ea44aece23a541b1b015d52b1babe77ac8748
Никита <@DESKTOP-J9JNKJ9>
2023-02-18 03:54:22 +0200

    1

--- Registreerimine ja autoriseerimine_Konjajev/MyModule.py
+++ Registreerimine ja autoriseerimine_Konjajev/MyModule.py
@@ -1,12 +1,12 @@
 from random import *
-def kasutajaandmed():
+def kasutajaandmed(ll:list,p:list):
     print("Kas sa tahad oma andmet või random")
     a=input("Mida te soovite: ")
     if a=="oma":
-        login=input("Kirjutage teie nimi: ")       
-        psword=input("Kirjutage teie salasõna min6 värtused ja max 12 värtused: ")
+        login=input("Kirjutage teie nimi: ")      
+        psword=input("Kirjutage teie salasõna max 12 värtused: ")
         n=len(psword)
-        while n>5 and n<12:
+        while n<12:
             psword=input("Teie salasõna on lühike. Palun kirjuta veel: ")
             n=len(psword)
     elif a=="random":
@@ -24,6 +24,8 @@ def kasutajaandmed():
         #print(ls)
         # Извлекаем из списка 12 произвольных значений
         psword = ''.join([choice(ls) for x in range(12)])
+    ll.append(login)
+    p.append(psword)
     return login,psword
  
 def aut(ll:list,p:list):
@@ -31,7 +33,7 @@ def aut(ll:list,p:list):
     logg=input("Login: ")
     pas_=input("Salasõna: ")
     if logg in ll and pas_ in p:
-        print("Tere tulemast")
+        print("Tere tulemast!")
     else:
         print("Ebaõiged andmed")
     return logg, pas_
--- Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
+++ Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
@@ -1,19 +1,19 @@
 from MyModule import *
 #Registreerimine ja autoriseerimine
 
-ll=["user1","user2","user3"]
-p=["123","1234","321"]
+ll=[]
+p=[]
 while True:
     print("Kas sa reg või aut või välja")
     valik=input("Mida te tahate: ")
     if valik=="reg":
          print("Registreerimine")
-         login,psword=kasutajaandmed()
+         login,psword=kasutajaandmed(ll,p)
          print(f"Teie login ({login}) ja password ({psword})")
     elif valik=="aut":
-        print("autoriseerimine") 
-        log,pas=aut(ll,p)
+        print("autoriseerimine")
+        logg,pas_=aut(ll,p)
     elif valik=="välja":
         print("Nägemist")
         break
-        
+    print(ll,p)
