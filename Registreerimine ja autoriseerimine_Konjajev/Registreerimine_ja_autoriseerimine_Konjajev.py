8ce372255114b56f0e9b5360d171465e1d263103
NikitaKonjajev <120027766+NikitaKonjajev@users.noreply.github.com>
2023-02-20 12:21:11 +0200

    Update Registreerimine_ja_autoriseerimine_Konjajev.py

--- Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
+++ Registreerimine ja autoriseerimine_Konjajev/Registreerimine_ja_autoriseerimine_Konjajev.py
@@ -4,16 +4,33 @@ from MyModule import *
 ll=[]
 p=[]
 while True:
-    print("Kas sa reg või aut või välja")
-    valik=input("Mida te tahate: ")
-    if valik=="reg":
+    print("Registreerimine - 1")
+    print("autoriseerimine - 2")
+    print("Välja - 3")
+    print("muuta kasutajanime - 4")
+    print("muuta salasõna - 5")
+    print("Kui sa tahad vadata teie salasõna - 6")
+    valik=input("Kirjuta number, mis teeb su tegevuse: ")
+    if valik=="1":
          print("Registreerimine")
          login,psword=kasutajaandmed(ll,p)
          print(f"Teie login ({login}) ja password ({psword})")
-    elif valik=="aut":
-        print("autoriseerimine")
+    elif valik=="2":
+        print("autoriseerimine") 
         logg,pas_=aut(ll,p)
-    elif valik=="välja":
+    elif valik=="3":
         print("Nägemist")
         break
+    elif valik=="4":
+        vananimi=input("Kirjuta teie vana nimi: ")
+        uusnimi=input("Kirjuta uue nimi, mida sa sooid: ")
+        uss_login(ll,vananimi,uusnimi)
+    elif valik=="5":
+        login=input("Krjuta teie login: ")
+        vanasalasõna=input("Kirjuta teie vana salasõna: ")
+        uussalasõna=input("Kirjuta uus salasõna: ")
+        uss_salasõna(ll,p,login,vanasalasõna,uussalasõna)
+    elif valik=="6":
+        nimiii=input("Kirjuta teie login, kellel sa tahad vadata salasõna: ")
+        nimii(ll,p,nimiii)
     print(ll,p)
